2d6853d490223025614dca6815bed516a183b80c
omar-bakr <omar.bakr@student.guc.edu.eg>
2020-01-21 15:21:41 +0200

    Updates

---
+++ precache-manifest.270965ed65d880d9df871c94ff6f18b4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d0069e7ff9e6f2d5d127984ab173c4e",
+    "url": "/ReactTask/index.html"
+  },
+  {
+    "revision": "b9b30210bf462b1d48e6",
+    "url": "/ReactTask/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "ce30989ebaaac751d967",
+    "url": "/ReactTask/static/css/main.fddcc306.chunk.css"
+  },
+  {
+    "revision": "b9b30210bf462b1d48e6",
+    "url": "/ReactTask/static/js/2.6cab9d31.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/ReactTask/static/js/2.6cab9d31.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ce30989ebaaac751d967",
+    "url": "/ReactTask/static/js/main.b74f812e.chunk.js"
+  },
+  {
+    "revision": "7cb54d063b659ae6df90",
+    "url": "/ReactTask/static/js/runtime-main.61c7654c.js"
+  }
+]);
